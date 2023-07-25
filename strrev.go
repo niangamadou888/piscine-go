c60e3dc0554e960c0ba6c8dd35dec8abf2878aed
Amadou Boubacar Niang <71797475+niangamadou888@users.noreply.github.com>
2023-07-25 13:06:04 +0000

    Update strrev.go

--- strrev.go
+++ strrev.go
@@ -2,8 +2,11 @@ package piscine
 
 func StrRev(s string) string {
 	byte_str := []rune(s)
-	for i, j := 0, len(byte_str)-1; i < j; i, j = i+1, j-1 {
-		byte_str[i], byte_str[j] = byte_str[j], byte_str[i]
+	long := len(byte_str)
+	world := ""
+	for i := long - 1; i >= 0; i-- {
+		world += string(byte_str[i])
 	}
-	return string(byte_str)
+	return world
 }
+
