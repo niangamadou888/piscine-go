0b8d82f04127a24db51f32b37e3c5fa0825aea04
Amadou Boubacar Niang <71797475+niangamadou888@users.noreply.github.com>
2023-07-25 13:04:38 +0000

    Update basicatoi.go

--- basicatoi.go
+++ basicatoi.go
@@ -1,16 +1,11 @@
 package piscine
 
 func BasicAtoi(s string) int {
-	arrayStr := []rune(s)
-	n := len(s)
-	ans := 0
-	for i := 0; i < n; i++ {
-		if arrayStr[i] < '0' || arrayStr[i] > '9' {
-			return ans
-		} else {
-			ans *= 10
-			ans += int(arrayStr[i]) - '0'
-		}
+	num := 0
+	for _, digitChar := range s {
+		digit := int(digitChar - '0') // Convert rune to integer
+		num = num*10 + digit
 	}
-	return ans
+	return num
 }
+
